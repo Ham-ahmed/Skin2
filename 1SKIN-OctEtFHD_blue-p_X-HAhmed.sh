fa49c1229c94de40349f54cc700722517535f8bc
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2022-10-08 05:34:22 +0200

    Add files via upload

---
+++ 1SKIN-OctEtFHD_blue-p_X-HAhmed.sh
@@ -0,0 +1,16 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/skin-octetfhdbp-x_2.0_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/Skins/main/e2-plugin-skin-octetfhdbp-x_2.0_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/skin-octetfhdbp-x_2.0_all.ipk
+wait
+sleep 2;
+exit 0
+
+
+
+
